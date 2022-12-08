6d669efc2e2edb3e23d7d729213f2db2f834acd0
Augs 3 <110700696+august-3141@users.noreply.github.com>
2022-12-08 14:46:06 +0000

    s p a c e

--- Python Stuff/file_library.py
+++ Python Stuff/file_library.py
@@ -1,14 +1,17 @@
 # I love file handling
 
 def opendoc(x):
+    
     open(f"{x}.txt", "w", encoding="UTF-8")
     return f"Opened {x}.txt"
 
 def writedoc(x, y):
+
     file = open(f"{x}.txt", "a", encoding="UTF-8")
     file.write(f"{y} \n")
     return f"Text saved on {x}.txt."
 
 def readdoc(x):
+
     file = open(f"{x}.txt", "r", encoding="UTF-8")
     return file.read()
