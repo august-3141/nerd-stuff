e4b0aec2998d8a24d693f9079cf4a3a7636fa7d7
Augs 3 <aug.harr3141@gmail.com>
2022-12-07 22:28:44 -0500

    fixed recursion

--- Node Stuff/big_numbers.js
+++ Node Stuff/big_numbers.js
@@ -4,14 +4,21 @@ console.log("Big Number Time");
 console.log("---------------");
 
 function big_num(x) {
+
   if (x > 0) {
+
     return(x + big_num(x - 1));
+
   } else {
+
     return 0;
+
   }
 }
 
 for (var n = 1; n < 100; n++) {
+
   const out = big_num(n);
   console.log(out);
+  
 }
