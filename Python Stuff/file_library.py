c48e0d1fe764d33e2c6b4d93210118d94ca4c59b
Augs 3 <110700696+august-3141@users.noreply.github.com>
2022-12-08 14:42:42 +0000

    fixed file library

--- Python Stuff/file_library.py
+++ Python Stuff/file_library.py
@@ -2,13 +2,13 @@
 
 def opendoc(x):
     open(f"{x}.txt", "w", encoding="UTF-8")
-    return (f"Opened {x}.txt")
+    return f"Opened {x}.txt"
 
 def writedoc(x, y):
     file = open(f"{x}.txt", "a", encoding="UTF-8")
     file.write(f"{y} \n")
-    return (f"Text saved on {x}.txt.")
+    return f"Text saved on {x}.txt."
 
 def readdoc(x):
     file = open(f"{x}.txt", "r", encoding="UTF-8")
-    return (file.read())
+    return file.read()
