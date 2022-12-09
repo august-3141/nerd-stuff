13802e683073a116db88a5d2c090c7e0d4baa095
Augs 3 <110700696+august-3141@users.noreply.github.com>
2022-12-09 15:59:03 +0000

    lines

--- C++ Stuff/number_guesser.cpp
+++ C++ Stuff/number_guesser.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 int main() {
 
-    cout << "Hello";
+    int MAX = 10;
+    int guess;
+    int trueNum;
+
+    cout << "*** Number Guesser ***\n";
+    cout << "----------------------\n";
+
+    cout << "Enter your guess (1-10):\n";
+    cin >> guess;
+
+    srand(time(0));
+
+    trueNum = rand()%MAX;
+
+    cout << "The number was " << trueNum << ".\n";
+
+    if (trueNum == guess) {
+
+        cout << "You got the number right, good job!";
+
+    } else {
+
+        int amountOff = guess - trueNum;
+        amountOff = abs(amountOff);
+
+        cout << "Nice try, you were only " << amountOff << " off!";
+
+    }
+
     return 0;
 
 }
