c4a584daaf5e422ad94c93f2e0485a424a97eefd
Augs 3 <110700696+august-3141@users.noreply.github.com>
2022-12-09 17:36:40 +0000

    remember to fix launch.json

---
+++ C++ Stuff/psuedo_random.cpp
@@ -0,0 +1,22 @@
+// normal enough distribution
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+   std::default_random_engine pseudo_random_generator;
+   std::uniform_int_distribution<int> int_distribution(0, 9);
+   
+   int actual_distribution[10] = {0,0,0,0,0,0,0,0,0,0};
+   
+   for(int i = 0; i < 10000; i++) {
+       int result = int_distribution(pseudo_random_generator);
+       actual_distribution[result]++;
+   }
+
+   for(int i = 0; i <= 9; i++) {
+       std::cout << actual_distribution[i] << " ";
+   }
+}
