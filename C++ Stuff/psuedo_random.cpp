61918c9975b0100275af3b239341bc995b071bcc
Augs 3 <110700696+august-3141@users.noreply.github.com>
2022-12-09 17:39:05 +0000

    s p a c e

--- C++ Stuff/psuedo_random.cpp
+++ C++ Stuff/psuedo_random.cpp
@@ -10,17 +10,17 @@ int main() {
     cout << "*** Psuedo-random Number Counter ***" << endl;
     cout << "------------------------------------" << endl;
 
-   std::default_random_engine pseudo_random_generator;
-   std::uniform_int_distribution<int> int_distribution(0, 9);
+    std::default_random_engine pseudo_random_generator;
+    std::uniform_int_distribution<int> int_distribution(0, 9);
    
-   int actual_distribution[10] = {0,0,0,0,0,0,0,0,0,0};
+    int actual_distribution[10] = {0,0,0,0,0,0,0,0,0,0};
    
-   for(int i = 0; i < 10000; i++) {
+    for(int i = 0; i < 10000; i++) {
        int result = int_distribution(pseudo_random_generator);
        actual_distribution[result]++;
-   }
+    }
 
-   for(int i = 0; i <= 9; i++) {
+    for(int i = 0; i <= 9; i++) {
        std::cout << actual_distribution[i] << " ";
-   }
+    }
 }
